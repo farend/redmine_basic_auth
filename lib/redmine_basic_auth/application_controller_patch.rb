16da067155a3343d9eb9ecfd5973c8569bd16b90
MAEDA Go <maeda@farend.jp>
2017-10-12 11:32:32 +0900

    Fix compatibility problems with Redmine 3.4.

--- lib/redmine_basic_auth/application_controller_patch.rb
+++ lib/redmine_basic_auth/application_controller_patch.rb
@@ -8,17 +8,17 @@ module RedmineBasicAuth
     def self.included(base)
       base.send(:include, InstanceMethods)
       base.class_eval  do
-        before_filter :require_basic_auth
+        before_action :require_basic_auth
       end
     end
   end
 
   module InstanceMethods
     def require_basic_auth
-      return true if Setting.plugin_redmine_basic_auth[:enabled].to_i == 0
+      return true if Setting.plugin_redmine_basic_auth['enabled'].to_i == 0
       authenticate_or_request_with_http_basic(REALM) do |user_name, password|
-        user_name == Setting.plugin_redmine_basic_auth[:user_name] &&
-        password == Setting.plugin_redmine_basic_auth[:password]
+        user_name == Setting.plugin_redmine_basic_auth['user_name'] &&
+        password == Setting.plugin_redmine_basic_auth['password']
       end
     end
   end
